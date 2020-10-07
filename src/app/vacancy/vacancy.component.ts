e53627a50a2b2b1cfb9f3f05051343f71b8822e8
aditya <adityagautamm@rediffmail.com>
2020-10-07 19:34:39 +0530

    vacancy added

---
+++ src/app/vacancy/vacancy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vacancy',
+  templateUrl: './vacancy.component.html',
+  styleUrls: ['./vacancy.component.css']
+})
+export class VacancyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
