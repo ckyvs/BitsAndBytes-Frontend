a55aaae0d4c91f9ac2616fa1ece6de9daf7718e8
YVS Chakri <chakriyvs@gmail.com>
2020-10-08 12:32:41 +0530

    improved navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn:boolean
+  constructor(private authService:AuthenticationService) { }
 
   ngOnInit(): void {
+    this.isLoggedIn=this.authService.isLoggedIn();
   }
 
 }
